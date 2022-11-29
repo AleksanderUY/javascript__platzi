3694e4454e626ad01bac50ac877306436a7decf9
Aleksander Simanski <aleksandersimanski@outlook.com>
2022-11-29 10:10:27 -0300

    guardando concepto de funciones

---
+++ valores.js
@@ -0,0 +1,14 @@
+40 // numero
+
+"texto o strings" // strings
+
+// boolean
+true
+false
+
+null // reservado para un valor
+undefined // reservado para un valor
+
+
+[1,2,3] // array o arreglo
+{nombre: "diego"} // esto es un objeto o valor tipo objeto
---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = 'nombre';
+// asignamos la variable nombre un valor de tipo strings
+
+
+//declarar
+var edad;
+//inicializar
+edad = 30;
+
+edad 
+
+var elementos = ['computadora','celular'];
+
+var persona = {
+    nombre: 'diego',
+    edad: 30
+}
+
+persona 
