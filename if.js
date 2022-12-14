04ecc90f77ef3961cee2411c8135836131f5793d
Aleksander Simanski <aleksandersimanski@outlook.com>
2022-12-14 09:58:30 -0300

    agregando archivo html al repositorio para poder ejecutar pruebas

---
+++ if.js
@@ -0,0 +1,11 @@
+if (true) {
+    console.log("hola");
+}
+// si la condicion en el parametro asignado entre parentesis es verdadero da la bienvenida
+if (true) {
+    console.log("da la bienvenida");
+} 
+//si es falso ejecuta la linea else y muestra en consola que no puede ingresar
+else {
+console.log("no puedes ingresar");
+}
