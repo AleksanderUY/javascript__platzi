92e729c5dff011da5bbf76efcd2b1ff4671ef0cf
Aleksander Simanski <aleksandersimanski@outlook.com>
2022-12-14 10:39:15 -0300

    agregando metodo switch

--- if.js
+++ if.js
@@ -5,7 +5,45 @@ if (true) {
 if (true) {
     console.log("da la bienvenida");
 } 
+// esta condicion si no se cumple revisa la segunda condicion else if
+else if () {
+
+}
 //si es falso ejecuta la linea else y muestra en consola que no puede ingresar
 else {
 console.log("no puedes ingresar");
 }
+
+
+//operador ternario
+
+condition ? true : false;
+
+var numero = 1;
+
+var resultado = numero === 1? "Si soy un uno" : "No,no soy uno";
+
+/*var op1 = "Piedra";
+var op2 = "Papel";
+var op3 = "Tijera";
+
+var resultado = function(user, cpu){
+    if(user != cpu){
+        if(user === op1 && cpu === op3){
+            console.log("el usuario GANO con "+ op1)
+        }else if(user === op2 && cpu === op1){
+            console.log( "el usuario GANO con " + op2)
+        }else if(user === op3 && cpu === op2){
+            console.log("el usuario GANO con " + op3)
+        }else{
+            console.log("La CPU Gano!!")
+        }
+    }else if(user === cpu){
+        console.log("Empate")
+    }
+};
+
+resultado(op1,op3) //el usuario GANO con Piedra```*/
+
+
+
