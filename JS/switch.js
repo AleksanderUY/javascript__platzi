90bd88e0f5c80c167b76897a56d4a9b71aa1274b
Aleksander Simanski <aleksandersimanski@outlook.com>
2023-01-22 16:43:39 -0300

    agregando ejemplos practicos de sentencia switch

--- JS/switch.js
+++ JS/switch.js
@@ -14,3 +14,57 @@ switch (numero) {
         break;
 } */
 
+/* function clasificarValor(valor) {
+var respuesta;
+switch(valor){
+    case 1:
+        respuesta = "alpha";
+        break;
+    case 2:
+        respuesta = "beta";
+        break;
+    case 3:
+        respuesta = "gamma";
+        break;
+    case 3:
+        respuesta = "delta";
+        break
+}
+return respuesta;
+}
+
+console.log(calificarValor(1)); */
+
+/* var producto = "hambuguesa";
+
+switch(producto){
+    case "pizza":
+       onsole.log("La pizza cuesta $10.55");
+        break;
+    case "hambuguesa":
+        console.log("La hambuguesa cuesta $6.78");
+        break;
+        case "helado":
+            console.log("el helado cuesta $2.8");
+            break;
+} */
+/* function seleccionarIdioma(valor) {
+    var idioma;
+    switch (valor) {
+        case 1:
+            idioma = "espanol"
+                break;
+        case 2:
+            idioma = "frances"
+                break;
+        case 3:
+            idioma = "italiano"
+                break;
+        default:
+            idioma = "ingles"
+            break;
+    }
+    return idioma;
+}
+console.log(seleccionarIdioma(3)); */
+
